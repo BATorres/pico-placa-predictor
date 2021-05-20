ba8673cdb5ed66858367ed469f3e1569acdcbf53
Andrés Torres Albuja <bryan.torres01@epn.edu.ec>
2021-05-20 15:55:14 -0500

    Textos en inglés y corrección en pattern de placa

--- src/app/predictor/predictor.component.ts
+++ src/app/predictor/predictor.component.ts
@@ -13,7 +13,7 @@ export class PredictorComponent implements OnInit {
   public isPredicted = false;
   public canCirculate = false;
   public licensePlateMask = LICENSE_PLATE_MASK;
-  public licensePlatePattern = '[a-zA-Z]{3}-[1-9]{4}';
+  public licensePlatePattern = '[a-zA-Z]{3}-[0-9]{4}';
 
   constructor(private readonly formBuilder: FormBuilder) {}
 
@@ -46,52 +46,52 @@ export class PredictorComponent implements OnInit {
       switch (selectedDay) {
         case 0:
           if (lastDigit === 1 || lastDigit === 2) {
-            this.message = 'No puede circular';
+            this.message = 'Car can not be on the road';
             this.canCirculate = false;
           } else {
-            this.message = 'Puede circular';
+            this.message = 'Car can be on the road';
             this.canCirculate = true;
           }
           break;
         case 1:
           if (lastDigit === 3 || lastDigit === 4) {
-            this.message = 'No puede circular';
+            this.message = 'Car can not be on the road';
             this.canCirculate = false;
           } else {
-            this.message = 'Puede circular';
+            this.message = 'Car can be on the road';
             this.canCirculate = true;
           }
           break;
         case 2:
           if (lastDigit === 5 || lastDigit === 6) {
-            this.message = 'No puede circular';
+            this.message = 'Car can not be on the road';
             this.canCirculate = false;
           } else {
-            this.message = 'Puede circular';
+            this.message = 'Car can be on the road';
             this.canCirculate = true;
           }
           break;
         case 3:
           if (lastDigit === 7 || lastDigit === 8) {
-            this.message = 'No puede circular';
+            this.message = 'Car can not be on the road';
             this.canCirculate = false;
           } else {
-            this.message = 'Puede circular';
+            this.message = 'Car can be on the road';
             this.canCirculate = true;
           }
           break;
         case 4:
           if (lastDigit === 9 || lastDigit === 0) {
-            this.message = 'No puede circular';
+            this.message = 'Car can not be on the road';
             this.canCirculate = false;
           } else {
-            this.message = 'Puede circular';
+            this.message = 'Car can be on the road';
             this.canCirculate = true;
           }
           break;
       }
     } else {
-      this.message = 'Puede circular';
+      this.message = 'Car can be on the road';
       this.canCirculate = true;
     }
   }
